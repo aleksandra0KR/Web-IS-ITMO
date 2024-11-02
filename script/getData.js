86b72eae1e3201a4dcfd79e6f8c4306d210b91e6
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-11-02 14:06:24 +0300

    fix: delete innerHTML

--- script/getData.js
+++ script/getData.js
@@ -1,17 +1,25 @@
-(function() {
+(function () {
     const form = document.getElementById('userForm');
     const userDataDiv = document.getElementById('userData');
 
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
 
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
 
-        userDataDiv.innerHTML = `
-            <h3>Ваши данные:</h3>
-            <p>Имя: ${name}</p>
-            <p>Email: ${email}</p>
-        `;
+        userDataDiv.innerHTML = '';
+
+        const title = document.createElement('h3');
+        title.textContent = 'Ваши данные:';
+        userDataDiv.appendChild(title);
+
+        const nameParagraph = document.createElement('p');
+        nameParagraph.textContent = `Имя: ${name}`;
+        userDataDiv.appendChild(nameParagraph);
+
+        const emailParagraph = document.createElement('p');
+        emailParagraph.textContent = `Email: ${email}`;
+        userDataDiv.appendChild(emailParagraph);
     });
 })();
