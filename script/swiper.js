4f4a28ecd479c3f599d5aeafa0f1bc3a7a6d6959
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-12-18 11:45:18 +0300

    feat: add comments for 7 lab for swiper

--- script/swiper.js
+++ script/swiper.js
@@ -1,22 +1,12 @@
+// Инициализация Swiper слайдера после загрузки DOM
 document.addEventListener('DOMContentLoaded', function () {
      new Swiper('.swiper-container', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        loop: true,
-        centeredSlides: true,
+        slidesPerView: 1, // Количество слайдов, отображаемых одновременно
+        loop: true, // Включение бесконечного цикла
+        centeredSlides: true, // Центрирование активного слайда
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next', // Элемент для перехода к следующему слайду
+            prevEl: '.swiper-button-prev', // Элемент для перехода к предыдущему слайду
         },
-        observer: true,
-        observeParents: true,
-        preloadImages: false,
-        lazy: {
-            loadPrevNext: true,
-            loadOnTransitionStart: true,
-        },
-        updateOnImagesReady: true,
-        parallax: true,
     });
-
 });
